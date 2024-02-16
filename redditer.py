bbbf2970de3e493946e497019b740385015fd180
Valacor <valyrious.informatics@gmail.com>
2024-02-16 23:40:34 +0100

    Rework redditer output structure & implement filtering

--- redditer.py
+++ redditer.py
@@ -4,6 +4,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 import time
 import json
+import os 
 
 def prepare_reddit(browser: webdriver.Firefox, scrollDownCount: int):
     for i in range(0, scrollDownCount):
@@ -15,28 +16,40 @@ def get_links(browser: webdriver.Firefox):
     for img in imgs:
         yield [i.strip() for i in img.get_attribute("srcset").split(",")]
 
-def to_link_dictionary(links: list[str]):
-    res = {}
+def get_highest_link(links: list[str]) -> str:
+    highest = -1
+    link = ""
     for link in links:
         split = link.split(" ")
         if (len(split) == 1):
-            res[1] = split[0]
-            break
+            continue
         key = int(split[1].replace("w", "").strip())
-        res[key] = split[0]
-    return res 
+        if (key > highest):
+            highest = key
+            link = split[0]
+    return link.split(" ")[0].strip()
 
-def main(subreddit: str, storefile: str):
+def main(browser: webdriver.Firefox, subreddit: str, storefile: str):
     url = f"https://www.reddit.com/r/{subreddit}/"
-    browser = webdriver.Firefox(options=Options())
     browser.get(url)
-        
+
     prepare_reddit(browser, 20)
 
-    linkCollection = [to_link_dictionary(i) for i in get_links(browser)]
+    oldCollection = []
+    if (os.path.exists(storefile)):
+        with open(storefile, "r") as fobj:
+            oldCollection = list(json.load(fobj))
 
-    with open(storefile, "a") as fobj:
-        json.dump(linkCollection, fobj)
+    linkCollection = [d for d in (get_highest_link(i) for i in get_links(browser)) if d not in oldCollection and d != ""] 
+
+    for i in linkCollection:
+        print(i)
+
+    oldCollection.extend(linkCollection)
+
+    with open(storefile, "w") as fobj:
+        json.dump(oldCollection, fobj)
 
 if __name__ == "__main__":
-    main("programmingmemes", "res.json")
+    browser = webdriver.Firefox(options=Options())
+    main(browser, "programmingmemes", "res.json")
