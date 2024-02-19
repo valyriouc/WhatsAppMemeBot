b475c6a14808f1bb5be095138d7da5c1a1cc42bc
Valacor <valyrious.informatics@gmail.com>
2024-02-19 21:09:46 +0100

    Convert redditer logic into class

--- redditer.py
+++ redditer.py
@@ -6,51 +6,51 @@ import time
 import json
 import os 
 
-def prepare_reddit(browser: webdriver.Firefox, scrollDownCount: int):
-    for _ in range(0, scrollDownCount):
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        time.sleep(1)
+class Redditer(webdriver.Firefox):
+    def __init__(self, subreddit: str, scrollCount: int) -> None:
+        super().__init__(Options(), None, True)
+        self.get(f"https://www.reddit.com/r/{subreddit}/")
+        self.scrollCount = scrollCount
+
+    def _prepare_reddit(self):
+        for _ in range(0, self.scrollCount):
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            time.sleep(0.5)
     
-def get_links(browser: webdriver.Firefox):
-    imgs = browser.find_elements(By.XPATH, value='//img[contains(@alt, "r/programmingmemes")]')
-    for img in imgs:
-        yield [i.strip() for i in img.get_attribute("srcset").split(",")]
+    def _get_links(self):
+        imgs = browser.find_elements(By.XPATH, value='//img[contains(@alt, "r/programmingmemes")]')
+        for img in imgs:
+            yield [i.strip() for i in img.get_attribute("srcset").split(",")]
+    
+    @staticmethod
+    def _get_highest_link(links: list[str]) -> str:
+        highest = -1
+        link = ""
+        for link in links:
+            split = link.split(" ")
+            if (len(split) == 1):
+                continue
+            key = int(split[1].replace("w", "").strip())
+            if (key > highest):
+                highest = key
+                link = split[0]
+        return link.split(" ")[0].strip()
+    
+    def render(self):
+        self._prepare_reddit()
+        return [Redditer._get_highest_link(i) for i in self._get_links()]
 
-def get_highest_link(links: list[str]) -> str:
-    highest = -1
-    link = ""
-    for link in links:
-        split = link.split(" ")
-        if (len(split) == 1):
-            continue
-        key = int(split[1].replace("w", "").strip())
-        if (key > highest):
-            highest = key
-            link = split[0]
-    return link.split(" ")[0].strip()
-
-def main(browser: webdriver.Firefox, subreddit: str, storefile: str):
-    url = f"https://www.reddit.com/r/{subreddit}/"
-    browser.get(url)
-
-    prepare_reddit(browser, 20)
-
+def main(renderer: Redditer, storefile: str):
     oldCollection = []
     if (os.path.exists(storefile)):
         with open(storefile, "r") as fobj:
             oldCollection = list(json.load(fobj))
-
-    linkCollection = [d for d in (get_highest_link(i) for i in get_links(browser)) if d not in oldCollection and d != ""] 
-
-    for i in linkCollection:
-        print(i)
-
+    linkCollection = [d for d in renderer.render() if d not in oldCollection and d != ""] 
     oldCollection.extend(linkCollection)
-
     with open(storefile, "w") as fobj:
         json.dump(oldCollection, fobj)
 
 if __name__ == "__main__":
-    browser = webdriver.Firefox(options=Options())
-    main(browser, "programmingmemes", "memes.json")
+    browser = Redditer("programmingmemes", 20)
+    main(browser, "memes.json")
     browser.quit()
