d8df75f97910a18754a77f16498324e548e1e900
Valacor <valyrious.informatics@gmail.com>
2024-02-19 21:44:07 +0100

    Rework the whatsapper

--- whatsapper.py
+++ whatsapper.py
@@ -2,41 +2,45 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-import time
 import json
+import pyperclip3 as clip
 
-def main(browser: webdriver.Firefox, contact: str):
-    url = 'https://web.whatsapp.com/'
-    browser.get(url)
-    input("Press Enter after scanning QR code and logging in...")
+class Whatsapper(webdriver.Firefox):
+    def __init__(self, contacts: list[str], memes: list[str]) -> None:
+        super().__init__(Options(), None, True)
+        self.contacts = contacts
+        self.memes = memes
+        self.get("https://web.whatsapp.com/")
 
-    while True:
-        try:
-            search_box = browser.find_element(by=By.XPATH, value='//div[contains(@title, "Sucheingabefeld")]')
-            contact_name = contact 
-            for e in contact_name:
-                search_box.send_keys(e)
-            search_box.send_keys(Keys.ENTER)
-            time.sleep(1)
-            input_box = browser.find_element(by=By.XPATH, value='//div[contains(@title, "Gib eine Nachricht ein.")]')
+    def _search_for(self, contact):
+        search_box = self.find_element(by=By.XPATH, value='//div[contains(@title, "Sucheingabefeld")]')
+        clip.copy(contact)
+        search_box.send_keys(Keys.CONTROL+"v")
+        search_box.send_keys(Keys.ENTER)
+     
+    def _typing_in(self):
+        input_box = self.find_element(by=By.XPATH, value='//div[contains(@title, "Gib eine Nachricht ein.")]')
+        for meme in self.memes:
+            clip.copy(meme)
+            input_box.send_keys(Keys.CONTROL + "v")
+            input_box.send_keys(Keys.ENTER)
+    
+    def send(self):
+        input("Press Enter after scanning the QR code...")
+        for contact in self.contacts:
+            self._search_for(contact)
+            self._typing_in()
 
-            memes = None
-            with open("memes.json", "r") as fobj:
-                memes = json.load(fobj)
+def main():
+    memes = None
+    with open("memes.json", "r") as fobj:
+        memes = json.load(fobj)
 
-            for meme in memes:
-                for m in meme:
-                    input_box.send_keys(m)
-                input_box.send_keys(Keys.ENTER)
-            time.sleep(1)
-        
-        except KeyboardInterrupt:
-            print("Bot is shutting down!")
-            break
-        except:
-            continue
-
-if __name__ == "__main__":
-    browser = webdriver.Firefox(Options())
-    main(browser, "Ich")
+    browser = Whatsapper(["Ich"], memes)
+    browser.send()
     browser.quit()
+
+    print("Bot is done sending the memes!")
+        
+if __name__ == "__main__":
+    main()
